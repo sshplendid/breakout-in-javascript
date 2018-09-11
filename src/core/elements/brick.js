ef7137084ce57e6f3a8983e1bdd729f29009faad
SeHun Shin <s.shin@marathoner.kr>
2018-09-12 00:00:23 +0900

    feat: update brick
    인자가 options 객체인 경우 생성가능하도록 변경

--- src/core/elements/brick.js
+++ src/core/elements/brick.js
@@ -3,12 +3,21 @@
 
 class Brick {
   constructor(x, y, width, height, color, durability) {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.width = width || 0;
-    this.height = height || 0;
-    this.color = color || '#000000';
-    this.durability = durability || 0;
+    if(arguments.length > 0 && arguments[0] instanceof Object) {
+      this.x = arguments[0].x || 0;
+      this.y = arguments[0].y || 0;
+      this.width = arguments[0].width || 0;
+      this.height = arguments[0].height || 0;
+      this.color = arguments[0].color || '#000000';
+      this.durability = arguments[0].durability || 0;
+    } else {
+      this.x = x || 0;
+      this.y = y || 0;
+      this.width = width || 0;
+      this.height = height || 0;
+      this.color = color || '#000000';
+      this.durability = durability || 0;
+    }
   }
 
   crack() {
