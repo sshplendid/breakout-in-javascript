97182d58b96a5473da474eaa6b348979fc1a4078
SeHun Shin <s.shin@marathoner.kr>
2018-09-15 15:28:20 +0900

    feat: add brickRepository

--- src/core/repository/brick.repository.js
+++ src/core/repository/brick.repository.js
@@ -2,10 +2,16 @@
 
 const Brick = require('../elements/brick.js');
 const BRICKS = Symbol('BRICKS');
-const EMTPY = Symbol('EMPTY_BRICK');
+const ROW = Symbol('ROW');
+const COL = Symbol('COL');
 
 class BrickRepository {
 
+  constructor() {
+    this[BRICKS] = [];
+    this[ROW] = 0;
+    this[COL] = 0;
+  }
 
   createFreshBricks(row, col, options) {
     const newArray = [];
@@ -13,15 +19,54 @@ class BrickRepository {
         newArray.push(new Brick(options));
 
     this[BRICKS] = newArray;
+    this[ROW] = row;
+    this[COL] = col;
     return true;
   }
+
+  get row() { return this[ROW]; }
+  get col() { return this[COL]; }
+  get bricks() { return this[BRICKS]; }
+
   getExistingBrickCount() {
-    return this[BRICKS].filter(brick => brick.isNotBroken()).length;
+    return this.getAvailableBricks().length;
   }
 
   getAvailableBricks() {
-    return [];
+    return this.bricks.filter(brick => brick.isNotBroken());
   }
+
+  getIndex(x, y) {
+    if(y > this.row || x >> this.col) return -1;
+    return (y - 1) * this.row + (x - 1);
+  }
+
+  getBrick(x, y) {
+    return this.bricks[this.getIndex(x, y)];
+  }
+
+  setBrick(x, y, brick) {
+    return this.bricks[this.getIndex(x, y)] = brick;
+  }
+  
+  /*
+  makeBricks(rows, columns) {
+    if(this.isPlaying() ) return false;
+    const brickArray = [];
+
+    for(let r = 0; r < rows; r++) {
+      const rowArray = [];
+      for(let c = 0; c < columns; c++) rowArray.push(new Brick());
+      brickArray.push(rowArray);
+    }
+    this[BRICKS] = brickArray;
+    return true;
+  }
+
+  isPlaying() {
+    return this[PLAYING];
+  }
+  */
 }
 
 module.exports = BrickRepository;
--- src/core/repository/brick.repository.spec.js
+++ src/core/repository/brick.repository.spec.js
@@ -3,6 +3,16 @@
 const {assert} = require('chai');
 const BrickRepository = require('./brick.repository.js');
 
+const row = 3, col = 3;
+const options = {
+  x: 1,
+  y: 2,
+  width: 3,
+  height: 4,
+  color: 'black',
+  durability: 5
+};
+
 describe(`repository/BrickRepository`, () => {
   it(`constructor`, () => {
     const repo = new BrickRepository();
@@ -10,16 +20,69 @@ describe(`repository/BrickRepository`, () => {
   });
   it(`createFreshBricks`, () => {
     const repo = new BrickRepository();
-    const row = 3, col = 3;
-    const options = {
-      x: 1,
-      y: 2,
-      width: 3,
-      height: 4,
-      color: 'black',
-      durability: 5
-    };
     assert.isTrue( repo.createFreshBricks(row, col, options) );
-    assert.equal( 9, repo.getExistingBrickCount() );
+    assert.equal(repo.getExistingBrickCount(), 9);
+  });
+  
+  it(`getAvailableBricks`, () => {
+    const repo = new BrickRepository();
+    assert.isArray(repo.getAvailableBricks());
+  });
+  
+  it(`getIndex_r:${row},c:${col} 일 때_인덱스(1,3)는_2가 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(row, col, {}) );
+    
+    assert.equal(repo.getIndex(1, 3), 6);
+  });
+  it(`getIndex_r:${row},c:${col} 일 때_인덱스(3,3)는_8이 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(row, col, {}) );
+    assert.equal(repo.getIndex(col, row), 8);
+  });
+  it(`getIndex_r:${row},c:${col} 일 때_인덱스(1,1)는_0이 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(row, col, {}) );
+    assert.equal(repo.getIndex(1, 1), 0);
+  });
+  it(`getIndex_r:1,c:1 일 때_인덱스(1,1)는_0이 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(1, 1, {}) );
+    assert.equal(repo.getIndex(1, 1), 0);
+  });
+
+  it(`getIndex_r:1,c:1 일 때_인덱스(2,1)는_-1이 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(1, 1, {}) );
+    assert.equal(repo.getIndex(2, 1), -1);
+  });
+  it(`getIndex_r:1,c:1 일 때_인덱스(1,2)는_-1이 나온다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(1, 1, {}) );
+    assert.equal(repo.getIndex(1, 2), -1);
+  });
+  
+  it(`getBrick_r:${row}, c:${col}일 때_2번째 벽돌을 꺼내면_Brick ojbect를 리턴한다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(row, col, options) );
+    const brick = repo.getBrick(0, 2);
+    assert.isObject( brick );
+    assert.equal(brick.durability, options.durability);
+  });
+
+  it(`getBrick_r:${row}, c:${col}일 때_10번째 벽돌을 꺼내면_undefined를 리턴한다`, () => {
+    const repo = new BrickRepository();
+    assert.isTrue( repo.createFreshBricks(row, col, options) );
+    const brick = repo.getBrick(1, 4);
+    assert.isUndefined( brick );
+  });
+  it(`setBrick_r:${row}, c:${col}일 때_3번째 벽돌을 바꾸면_아무일도 일어나지 않는다`, () => {
+    const repo = new BrickRepository();
+    repo.createFreshBricks(row, col, options);
+    const brick = repo.getBrick(col, 1);
+    brick.crack();
+    assert.equal(options.durability - 1, brick.durability);
+    assert.deepEqual(repo.getBrick(col, 1), brick);
+    assert.equal(repo.getBrick(col, 1).durability, 4);
   });
 });
