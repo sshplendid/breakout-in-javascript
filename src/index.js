c4ca374258b94d8a5af8d5d8849dad3a55cda784
SeHun Shin <s.shin@marathoner.kr>
2018-09-15 22:09:07 +0900

    refactor: index.js
    BrickRepository로 대체

--- src/index.js
+++ src/index.js
@@ -4,33 +4,59 @@ console.debug = (message) => console.log(`[DEBUG] ${message}`);
 console.trace = () => true;
 
 // load source
-const Configuration = require('./core/config/config.js');
-const Brick = require('./core/elements/brick.js');
+const BrickRepository = require('./core/repository/brick.repository.js');
 const Ball = require('./core/elements/ball.js');
 const Paddle = require('./core/elements/paddle.js');
 const Painter = require('./render/painter.js');
+const Renderer = require('./render/renderer.js');
 
 // config
-const options = {
+const CONF = {
+  windowWidth: window.innerWidth,
+  windowHeight: window.innerHeight,
   canvasWidth: 450,
   canvasHeight: 600,
-  paddleTop: 100,
+  paddle: {
+    top: 500,
+    left: 0,
+    width: 120,
+    height: 10,
+    color: 'rgba(36, 68, 38, 0.8)',
+    dx: 3
+  },
+  bricks: {
+    row: 4,
+    col: 4,
+    width: 80,
+    height: 20,
+    durability: 3,
+    colors: ['rgba(0,0,0,0.00)', 'red', 'orange', 'green'],
+    padding: 30,
+    offset: {
+      top: 100,
+      left: 25
+    }
+  },
+  balls: {
+
+  }
 };
-const CONF = new Configuration(
-  options.canvasWidth, 
-  options.canvasHeight, 
-  options.paddleTop);
 
 
 // init
+var app = document.createElement('div');
+app.setAttribute('id', 'app');
+app.setAttribute('class', 'app full');
+
 var canvas = document.createElement('canvas');
 canvas.width = CONF.canvasWidth;
 canvas.height = CONF.canvasHeight;
+app.appendChild(canvas);
 
 const painter = new Painter(canvas, 0, 0, CONF.canvasWidth, CONF.canvasHeight);
 const paddle = new Paddle(250, 500, 120, 10, 'rgba(36, 68, 38, 0.8)', 3);
 
-document.body.appendChild(canvas);
+document.body.appendChild(app);
 var ctx = canvas.getContext('2d');
 
 var rightPressed = false;
@@ -68,8 +94,8 @@ function BallManager(number) {
         console.trace(`ball{x:${this.x}, y:${this.y}}`);
         if(this.y - this.radius + this.dy <= 0) {
           this.dy = this.dy * (0-1);
-        } else if(this.y + this.radius + this.dy >= canvas.height - CONF.paddleTop) {
-          if(this.y < canvas.height - CONF.paddleTop && this.x > paddle.x && this.x <= paddle.x + paddle.width) {
+        } else if(this.y + this.radius + this.dy >= canvas.height - CONF.bricks.offset.top) {
+          if(this.y < canvas.height - CONF.bricks.offset.top && this.x > paddle.x && this.x <= paddle.x + paddle.width) {
             this.dy = this.dy * (0-1);
           }
         }
@@ -83,67 +109,41 @@ function BallManager(number) {
         this.x = this.x + this.dx;
         this.y = this.y + this.dy;
       };
-      balls[i]._detectCollision = function(bricks) {
-        var _c = bricks.length;
-        for(var c = 0; c < _c; c++) {
-          var _r = bricks[c].length;
-          for(var r = 0; r < _r; r++) {
-            var b = bricks[c][r];
-            if(b.durability > 0 && this.x + this.radius > b.x && this.x + this.radius < b.x + b.width
-                && this.y + this.radius > b.y && this.y + this.radius < b.y + b.height) {
-              // TODO 벽돌 안에 들어가는 버그 수정하기
-              if(this.x < b.x )
-                this.dx = (-1)*this.dx;
-              else
-                this.dy = ((-1)*this.dy);
-              b._collision();
-            }
-          }
-        }
-      };
     } /*-------- LEGACY END ---------------------------*/
   }
 
+  
   this.get = i => balls[i] || 0;
   this.getAll = () => balls;
   this.length = balls.length;
   this.allGone = () => balls.filter(ball => ball.y - ball.radius > canvas.height || ball.x - ball.radius < 0 || ball.x  + ball.radius > canvas.height).length  == balls.length;
 }
 
-function BrickFactory(_r, _c, _w, _h, _color, _padding, _offsetTop, _offsetLeft) {
-  var color = [undefined, 'red', 'orange', 'green'];
-  var bricks = [];
-  for(var c = 0; c < _c; c++) {
-    bricks[c] = [];
-    for(var r = 0; r < _r; r++) {
-      var brickX = c * (_w + _padding) + _offsetLeft;
-      var brickY = r * (_h + _padding) + _offsetTop;
-      var durability = Math.ceil(Math.random()*3) ;
-      // bricks[c][r] = new _Brick(brickX, brickY, _w, _h, color[durability], durability);
-      bricks[c][r] = new Brick(brickX, brickY, _w, _h, color[durability], durability);
-      /* ----------- LEGACY ---------------- */
-      bricks[c][r].Colors = ['rgba(0,0,0,0.00)', 'red', 'orange', 'green'];
-      bricks[c][r]._collision = function() {
-        this.crack();
-        this.color = this.Colors[this.durability];
-        console.debug(`[COLLISION] (${this.x}, ${this.y}) => ${this.durability}`);
-      };
-      /* ----------- LEGACY ---------------- */
-    }
-  }
-
-  this.getBricks = () => bricks;
-  this.getBrick = (x, y) => bricks[x][y];
-  BrickFactory.prototype.drawBricks = () => {
-    for(var c = 0; c < _c; c++) {
-      for(var r = 0; r < _r; r++) {
-        if(bricks[c][r].durability() > 0)
-          bricks[c][r].draw();
+const detectCollision = (ball, bricks) => {
+  // balls.forEach(ball => {
+    // TODO 충돌 감지 코드 작성!!
+    bricks.forEach(brick => {
+      if(brick.durability > 0 && ball.x + ball.radius > brick.x && ball.x + ball.radius < brick.x + brick.width
+        && ball.y + ball.radius > brick.y && ball.y + ball.radius < brick.y + brick.height) {
+        // TODO 벽돌 안에 들어가는 버그 수정하기
+        if(ball.x < brick.x )
+          ball.dx = (-1)*ball.dx;
+        else
+          ball.dy = ((-1)*ball.dy);
+        
+        crush(brick, CONF.bricks.colors); // brick._collision();
       }
-    }
-  };
-}
+    });
+  // });
+};
 
+
+
+function crush(brick, colors) {
+  brick.crack();
+  brick.color = colors[brick.durability];
+  return true;
+}
   function isOver(ballManager, intervalId) {
     if(ballManager.allGone()) {
       alert('GAME OVER');
@@ -155,40 +155,47 @@ function BrickFactory(_r, _c, _w, _h, _color, _padding, _offsetTop, _offsetLeft)
 
   
   { // LEGACY CODE --------------------------------->
-
-    
-    
     var ballManager = new BallManager(2);
-    var bricks = new BrickFactory(4, 4, 80, 20, 'lightgray', 30, CONF.paddleTop, 25);
-
-    // Board Legacy 대응
-    paddle._move = function() {
-      if( rightPressed && this.isNotOnRightBoundary(canvas.width) ) {
-        this.move();
-      } else if( leftPressed && this.isNotOnLeftBoundary(0) ){
-        this.move(true);
-      } 
-    };
-    // LEGACY CODE END --------------------------------->
+  }
+  var bricks = new BrickRepository(CONF.bricks);
+  function movePaddle(sigLeft, sigRight, paddle, config) {
+    if(sigLeft && paddle.isNotOnLeftBoundary(0)) return paddle.move(true);
+    else if(sigRight && paddle.isNotOnRightBoundary(config.canvasWidth)) return paddle.move(false);
+    return undefined;
+  }
+  function startDrawCanvas(_painter, _bricks, _balls, _paddle, _config) {
+    const intervalId = setInterval(function() {
+      _painter.clear(0, 0, _config.canvasWidth, _config.canvasHeight);
+  
+      // bricks.drawBricks();
+      /* LEGACY
+      _bricks.getBricks().forEach(brickRow => {
+        brickRow.forEach(brick => {
+          painter.draw(brick);
+        });
+      });
+      */
+     _bricks.getBricks().forEach(brick => {
+       console.debug(`draw brick: ${brick.x}, ${brick.y}`);
+       painter.draw(brick);
+     });
+      _balls.getAll().forEach(ball => {
+        detectCollision(ball, bricks.getBricks());// ball._detectCollision(_bricks.getBricks());
+        ball._move(_paddle);
+        _painter.draw(ball);
+      });
+      movePaddle(leftPressed, rightPressed, _paddle, _config);
+      _painter.draw(_paddle);
+      isOver(_balls, intervalId);
+    }, 10);
   }
 
-
-  var intervalId = setInterval(function() {
-    painter.clear(0, 0, canvas.width, canvas.height);
-
-    // bricks.drawBricks();
-    bricks.getBricks().forEach(brickRow => {
-      brickRow.forEach(brick => {
-        painter.draw(brick);
-      });
-    });
-    ballManager.getAll().forEach(ball => {
-      ball._detectCollision(bricks.getBricks());
-      ball._move(paddle);
-      painter.draw(ball);
-    });
-    paddle._move();
-    painter.draw(paddle);
-    isOver(ballManager, intervalId);
-  }, 10);
-// })();
+  var isPlaying = false;
+  document.body.onclick = () => {
+    if(isPlaying) return;
+    startDrawCanvas(painter, bricks, ballManager, paddle, CONF);
+    isPlaying = true;
+    console.log(`game started`);
+  };
+  console.log(`refactored: v1.4`);
+  
